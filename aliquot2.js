0720c142d74e70a9609f49d9031094d589f76569
Denny Vrandečić <vrandecic@gmail.com>
2024-01-27 22:50:34 -0800

    Create aliquot2.js

---
+++ aliquot2.js
@@ -0,0 +1,34 @@
+const divisors = (integer) => {
+  const result = []
+  for(let i = 1; i < integer; i++) {
+    if(integer % i == 0) result.push(i)
+  }
+  return result
+}
+
+const sum = x => x.reduce(
+  (partialSum, i) => partialSum + i, 0
+)
+
+const s = (integer) => sum(divisors(integer))
+
+const aliquot = (integer) => {
+  let steps = 0
+  const previous = []
+  let value = integer
+  console.log(steps, value)
+  let lastTime = new Date().getTime()
+  let currentTime = new Date().getTime()
+
+  while (true) {
+    steps += 1
+    previous.push(value)
+    value = s(value)
+    currentTime = new Date().getTime()
+    console.log(steps, value, currentTime-lastTime, 'ms')
+    lastTime = currentTime
+    if (previous.includes(value)) break
+  }
+}
+
+aliquot(276)
